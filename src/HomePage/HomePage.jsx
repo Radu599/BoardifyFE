bd5f94915f3fd9b47f41b095fbc2dd284a9edd70
Radu599 <30553913+Radu599@users.noreply.github.com>
2020-03-11 11:01:00 +0200

    GetAll games is working!

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -3,33 +3,26 @@ import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {gameActions} from '../_actions';
 import PrimarySearchAppBar from "../_components/layout/PrimarySearchAppBar";
+import Album from "../_components/Album";
 
 class HomePage extends React.Component {
-    componentDidMount() {
+    componentWillMount() {
         this.props.getGames();
     }
 
     render() {
         const {user, games} = this.props;
+
+
+
         return (
             <div>
+
                 <PrimarySearchAppBar></PrimarySearchAppBar>
                 <div className="col-md-6 col-md-offset-3">
-                    <h1>Hi {user.firstName}!</h1>
-                    <h3>All games:</h3>
-                    {games.loading && <em>Loading games...</em>}
-                    {games.error && <span className="text-danger">ERROR: {games.error}</span>}
-                    {games.items &&
-                    <ul>
-                        {games.items.map((game, index) =>
-                            <li key={user.id}>
-                                {game.id + ' ' + game.name}
-                                {
-                                }
-                            </li>
-                        )}
-                    </ul>
-                    }
+
+                    {games.items && <Album games={games} />}
+
                     <p>
                         <Link to="/login">Logout</Link>
                     </p>
